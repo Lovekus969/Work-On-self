caac386d487edb1e138051f3c6a890594a48d819
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-06-07 13:54:07 -0400

    Update greet.js

--- greet.js
+++ greet.js
@@ -4,3 +4,14 @@ function greet(name, age) {
 }
 
 greet("Kush"); // Output: Hello Kush, you are undefined years old.
+// How to Solve It
+function greet(name, age) {
+    if (name === undefined || age === undefined) {
+        console.log("Error: Missing name or age");
+        return;
+    }
+
+    console.log("Hello " + name + ", you are " + age + " years old.");
+}
+
+greet("Kush"); // Error: Missing name or age
